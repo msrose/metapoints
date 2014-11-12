0e74996979749c4a382347c79c35ecd0b9a08238
Michael Rose <ekimsc1094@sympatico.ca>
2014-11-12 17:03:07 -0500

    Add desktop notifications for chat messages.

--- chat.js
+++ chat.js
@@ -1,9 +1,25 @@
-angular.module("metapoints").controller("chat", ["$scope", "socket",
-  function($scope, socket) {
+angular.module("metapoints").controller("chat", ["$scope", "socket", "notification",
+  function($scope, socket, notification) {
     $scope.messages = [];
+
+    socket.on("me data", function(data) {
+      $scope.me = data.name;
+    });
 
     socket.on("chat message", function(data) {
       $scope.messages.push(data);
+      if(!notification.timedOut() && data.text.toUpperCase().indexOf("@" + $scope.me.toUpperCase()) >= 0 && data.sender !== $scope.me) {
+        notification.notify({
+          title: "Message from " + data.sender,
+          body: data.text,
+          dismiss: 15000,
+          timeout: 15000
+        }, function(notification) {
+          notification.onclick = function(e) {
+            window.focus();
+          };
+        });
+      }
       var messageList = document.getElementById("messagelist");
       messageList.scrollTop = messageList.scrollHeight;
     });
