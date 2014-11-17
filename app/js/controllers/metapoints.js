3709cfa55388e0d0f15d679409c6bc20c5827d3d
Michael <ekimsc1094@sympatico.ca>
2014-11-17 01:36:34 -0500

    Add keyboard shortcuts.

--- app/js/controllers/metapoints.js
+++ app/js/controllers/metapoints.js
@@ -13,6 +13,7 @@ angular.module("metapoints").controller("metapoints", ["$scope", "socket", "poin
     $scope.authQuestion = "";
     $scope.useMultiplier = false;
     $scope.cost = 0;
+    $scope.showShortcuts = false;
 
     socket.on("timeout change", function(data) {
       $scope.timeout = data.timeout;
@@ -39,6 +40,32 @@ angular.module("metapoints").controller("metapoints", ["$scope", "socket", "poin
       }
       $scope.cost = Math.round(selectedPointValue * ($scope.useMultiplier ? $scope.multiplier : 1) / 10);
     }, true);
+
+    $scope.shortcuts = [
+      { keys: "Alt+Up", action: "Increase Metapoints" },
+      { keys: "Alt+Down", action: "Decrease Metapoints" },
+      { keys: "Alt+M", action: "Toggle Multiplier" },
+      { keys: "Alt+Shift+?", action: "Toggle Shortcut List" }
+    ];
+
+    $scope.handleShortcut = function(e) {
+      if(e.altKey) {
+        switch(e.keyCode) {
+          case 38: //up
+            $scope.changeMetapoints('inc');
+            break;
+          case 40: //down
+            $scope.changeMetapoints('dec');
+            break;
+          case 77: //m
+            $scope.useMultiplier = !$scope.useMultiplier;
+            break;
+          case 191:
+            if(e.shiftKey) $scope.showShortcuts = !$scope.showShortcuts;
+            break;
+        }
+      }
+    };
 
     $scope.changeMetapoints = function(type) {
       if($scope.authAnswer) {
