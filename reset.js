f1fb2ea6e6a738d20862fc2a3302a18ff3fb05f1
Michael <ekimsc1094@sympatico.ca>
2014-11-27 21:50:52 -0500

    Reset messages as well as people.

--- reset.js
+++ reset.js
@@ -1,10 +1,12 @@
 var schemas = require("./schemas.json").schemas;
 
-var saveFile = process.argv[2] || "./points.json";
+var config = require(process.argv[2] || "./config.json");
+var db = require("./lib/filedb");
 
-var people = require("./lib/filedb")(saveFile, schemas.people, function(err) {
+var people = db(config.pointsFile, schemas.people, function(err) {
   if(err) {
     console.error("Failed to init people:", err);
+    return process.exit(1);
   }
 });
 
@@ -18,7 +20,27 @@ people.all().forEach(function(person) {
 
 people.save(function(err) {
   if(err) {
-    return console.error("Could not save!", err);
+    console.error("Could not save!", err);
+    return process.exit(1);
   }
   console.log("Reset people.");
+});
+
+var messages = db(config.messagesFile, schemas.messages, function(err) {
+  if(err) {
+    console.error("Could not load messages.");
+    return process.exit(1);
+  }
+});
+
+messages.all().forEach(function(message) {
+  messages.remove(message);
+});
+
+messages.save(function(err) {
+  if(err) {
+    console.error("Could not save message!", err);
+    return process.exit(1);
+  }
+  console.log("Reset messages.");
 });
